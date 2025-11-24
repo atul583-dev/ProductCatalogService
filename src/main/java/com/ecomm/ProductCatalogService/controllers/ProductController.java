a660cd59db94c2c4faf90b45dfdef57a4668a8f4
atul583-dev <atul583b@gmail.com>
2025-11-24 18:04:52 +0530

    delete target

--- src/main/java/com/ecomm/ProductCatalogService/controllers/ProductController.java
+++ src/main/java/com/ecomm/ProductCatalogService/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.ecomm.ProductCatalogService.controllers;
 
+import com.ecomm.ProductCatalogService.models.Product;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController

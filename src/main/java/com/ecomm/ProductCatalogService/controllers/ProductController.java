9e3c16b0b1c924b757827b2a6a92feb484085827
atul583-dev <atul583b@gmail.com>
2025-11-24 17:54:16 +0530

    first commit

---
+++ src/main/java/com/ecomm/ProductCatalogService/controllers/ProductController.java
@@ -0,0 +1,8 @@
+package com.ecomm.ProductCatalogService.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    
+}
---
+++ src/main/java/com/ecomm/ProductCatalogService/models/Product.java
@@ -0,0 +1,5 @@
+package com.ecomm.ProductCatalogService.models;
+
+public class Product {
+
+}

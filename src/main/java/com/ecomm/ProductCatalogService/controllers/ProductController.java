160296d7ad34f6c1c475b5d8954068e10ec2fe9a
atul583-dev <atul583b@gmail.com>
2025-11-24 18:01:04 +0530

    second commit

--- src/main/java/com/ecomm/ProductCatalogService/controllers/ProductController.java
+++ src/main/java/com/ecomm/ProductCatalogService/controllers/ProductController.java
@@ -4,5 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-    
+
+    public Product getProductDetails(Long id) {
+        Product product = new Product();
+        product.setId(id);
+        return product;
+    }
 }
